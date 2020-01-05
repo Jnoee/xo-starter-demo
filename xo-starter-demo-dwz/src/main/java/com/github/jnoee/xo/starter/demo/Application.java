55d3eb2d8bb86b3b04bf784741ada14a7714b95f
jnoee <jnoee@DESKTOP-RP42424.mshome.net>
2020-01-05 12:07:40 +0800

    简化配置。

--- xo-starter-demo-dwz/src/main/java/com/github/jnoee/xo/starter/demo/Application.java
+++ xo-starter-demo-dwz/src/main/java/com/github/jnoee/xo/starter/demo/Application.java
@@ -2,14 +2,12 @@ package com.github.jnoee.xo.starter.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.github.jnoee.xo.jpa.dao.DaoScan;
 
 @SpringBootApplication
 @EnableJpaRepositories
-@EntityScan
 @DaoScan
 public class Application {
   public static void main(String[] args) {
